c0d3e570b3c3cdd7b54da4a89b74d3634e7d2869
Nghi <nghintph28004@fpt.edu.vn>
2022-11-15 16:09:25 +0700

    pdo

--- dao/pdo.php
+++ dao/pdo.php
@@ -5,7 +5,7 @@
  */
 function pdo_get_connection()
 {
-    $dburl = "mysql:host=localhost;dbname=xshop;charset=utf8";
+    $dburl = "mysql:host=localhost;dbname=da1;charset=utf8";
     $username = 'root';
     $password = '';
 
